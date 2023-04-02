6d38bde2f732f9170eee05608bd5557350b45a1c
Mukund Kumar Verma <21mukund.29@gmail.com>
2023-04-02 13:09:29 +0530

    commit14

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 
 const modules = {
   toolbar: [
@@ -36,6 +37,7 @@ export default function CreatePost() {
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState(''); 
+  const [redirect,setRedirect] = useState(false);
 
   async function CreateNewPost(ev) {
     const data = new FormData();
@@ -51,7 +53,14 @@ export default function CreatePost() {
       body: data,
       // credentials: "include",
     });
-    console.log(response.json);
+    // console.log(response.json);
+    if(response.ok){
+      setRedirect(true);
+    }
+  }
+
+  if(redirect){
+    return <Navigate to={'/'} />
   }
 
   return (

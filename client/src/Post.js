1e16a9ee072609d2aabd86240d00aff4e92af267
Mukund Kumar Verma <21mukund.29@gmail.com>
2023-04-04 08:44:30 +0530

    commit16

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,26 +1,36 @@
 import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Post({title,summary,cover,content,createdAt,author}){
-    return (
-        <div className='post'>
-        <div className='image'>
-
-          <img src={'http://localhost:4000/'+cover} alt='blog_image'></img>
-        </div>
-
-        <div className='texts'>
-          <h2>{title}</h2>
-          <p className='info'>
-            {/* author?.username is used when we aren't sure that user property exits or not */}
-            <a className='author'>{author?.username}</a>
-            <time>{format(new Date(createdAt),'MMM do, yyy HH:mm')}</time>
-          </p>
-          <p className='summary'>
-            {summary}
-          </p>
-        </div>
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={"/post/" + _id}>
+          <img src={"http://localhost:4000/" + cover} alt="blog_image"></img>
+        </Link>
       </div>
-    );
+
+      <div className="texts">
+        <Link to={"/post/" + _id}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          {/* author?.username is used when we aren't sure that user property exits or not */}
+          <a className="author">{author?.username}</a>
+          <time>{format(new Date(createdAt), "MMM do, yyy HH:mm")}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 // export default Post;
